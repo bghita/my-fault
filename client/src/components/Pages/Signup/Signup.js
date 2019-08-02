769f63e1086c9ee0fed1cbf6ec6d9e03dfa1574f
Becky Ghita <ghita109@gmail.com>
2019-08-01 17:57:28 -0700

    adding carousel to sign in

--- client/src/components/Pages/Signup/Signup.js
+++ client/src/components/Pages/Signup/Signup.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Box from "../../Box/Box";
 import "./Signup.css";
+import { Carousel } from 'react-bootstrap';
+
 
 const Signup = () => {
     return (
         <div className="background">
-            
+            <Carousel/>
         </div>
     )
 }
