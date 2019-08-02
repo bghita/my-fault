f6cb858d62a553f6f626c0e9b34d33f4a14eb0cc
Kira Corbin <madkatkeys@gmail.com>
2019-08-02 15:14:56 -0700

    updating resource cards

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/NavBar/NavBar";
 import Signup from './components/Pages/Signup/Signup';
 import Login from './components/Pages/Login/Login';
+import Resources from "./components/Pages/Resources/Resources";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" component={Navbar}/>
         <Route exact path="/signup" component={Signup}/>
         <Route exact path="/login" component={Login}/>
+        <Route exact path="/resources" component={Resources}/>
     </Router>
   );
 }
--- client/src/components/Pages/ResourceCard/ResourceCard.js
+++ client/src/components/Pages/ResourceCard/ResourceCard.js
@@ -1,16 +1,19 @@
 // sets up Resource Cards for Resource Page
 import React from "react";
 
-
 const ResourceCard = (props) => {
     return (
         <div className="col-md-4">
             <div class="card" style={{ width: "18em" }}>
-                <img class="card-img-top" src={props.imgUrl} alt="Card cap" />
-                <div className="header">
-                    <h1 class="card-title">{props.imgUrl}</h1>
-                    <p class="card-body">body text</p>
-                    <a href="#" class="btn btn-primary"></a>
+
+                <a href={props.href}>
+                    <img class="card-img-top" src={props.imgUrl} alt="Card cap" />
+                </a>
+                
+                <div class="title text-center">{props.title}</div>
+                <div className="h1">
+                    <p class="body" style={{ fontSize: "14px" }}>{props.description}</p>
+                   {/* <a href="#" class="btn btn-primary"></a> */}
                 </div>
             </div>
         </div>
