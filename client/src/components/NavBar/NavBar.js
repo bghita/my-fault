3af01137fdfd3ac719b9f4c35fae4dafee990e44
Becky Ghita <ghita109@gmail.com>
2019-08-06 13:41:53 -0700

    fixed navbar logout button

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Navbar, Button, Nav } from "react-bootstrap";
+import { Navbar, Button, Nav, Col, Row, Container } from "react-bootstrap";
 import './NavBar.css';
 
 const NavBar = (props) => {
     return (
         <div>
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand id="Nav">My Fault</Navbar.Brand>
-            <Nav className="button">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/resources">Resources</Nav.Link>
-            </Nav>
-                <Button variant="outline-primary" href="/login" id="logout">Log Out</Button>
-        </Navbar>
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand id="Nav">My Fault</Navbar.Brand>
+                <Nav className="button">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/resources">Resources</Nav.Link>
+                </Nav>
+                <Button className="ml-auto" variant="outline-primary" href="/login">Log Out</Button>
+            </Navbar>
         </div>
     )
 }
