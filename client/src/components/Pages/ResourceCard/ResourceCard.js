b347af723ca537dbb34e182d1e04699a83642546
Kira Corbin <madkatkeys@gmail.com>
2019-08-06 14:15:30 -0700

    finish resorce layout

--- client/src/components/Pages/ResourceCard/ResourceCard.js
+++ client/src/components/Pages/ResourceCard/ResourceCard.js
@@ -1,34 +1,50 @@
 // sets up Resource Cards for Resource Page
 import React from "react";
 import "./ResourceCard.css"
-import {Link} from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Card } from "react-bootstrap"
+
 
 const ResourceCard = (props) => {
     return (
-        <div className="col-md-4">
-            <div class="card" style={{ width: "18em" }}>
-
-                <a href={props.href}>
-                    <img className="card-img-top imgUrl" src={props.imgUrl} alt="Card cap" />
-                </a>
-
-                <div className="title text-center">{props.title}</div>
+        <Col md={4} xs={12} className="my-1">
+            <Card style={{ backgroundColor:"lightgrey", boarder: "", opacity: "50%" }}>
+                <Card.Img variant="top" src={props.imgUrl}  style={{width:"100%", height:"130px"}}/>
+                <Card.Title>{props.title}</Card.Title>
+                
                 <div className="h1">
 
-                    <ul className="body" style={{ fontSize: "16px", marginTop: "5px" }}>
-                        {props.description.map(sentence => 
-                        <li key={sentence.description}>
-                            <a href={sentence.url} target="_blank"> {sentence.description}</a>
-                        </li>
+                    <ul className="body" style={{ fontSize: "16px" }}>
+                        {props.description.map(sentence =>
+                            <li key={sentence.description}>
+                                <a href={sentence.url} target="_blank" rel="noopener noreferrer"> {sentence.description}</a>
+                            </li>
                         )}
-                    </ul>       
-
+                    </ul>
 
                 </div>
-            </div>
-        </div>
+            </Card>
+        </Col>
+        // <div className="col-md-4">
+        //     <div class="card" style={{ width: "18em" }}>
 
+        //         <a href={props.href}>
+        //             <img className="card-img-top imgUrl" src={props.imgUrl} alt="Card cap" />
+        //         </a>
+
+        //         <div className="title text-center">{props.title}</div>
+        //         <div className="h1">
+
+        //             <ul className="body" style={{ fontSize: "16px", marginTop: "5px" }}>
+        //                 {props.description.map(sentence =>
+        //                     <li key={sentence.description}>
+        //                         <a href={sentence.url} target="_blank"> {sentence.description}</a>
+        //                     </li>
+        //                 )}
+        //             </ul>
+
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 export default ResourceCard;
