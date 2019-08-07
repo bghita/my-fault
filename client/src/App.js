72f589f9c00d9d8251940db30aa72d2c736d21eb
iromero385 <iromero385@berkeley.edu>
2019-08-06 20:35:35 -0700

    forgot to add home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,30 @@
-import React                              from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar                             from "./components/NavBar/NavBar";
-import Signup                             from './components/Pages/Signup/Signup';
-import Signup                             from "./components/Pages/Signup/Signup";
-import Login                              from './components/Pages/Login/Login';
-import EarthquakesContainer               from './components/Services/EarthquakesContainer';
-import Resources                          from "./components/Pages/Resources2/Resources2";
-import Footer                             from "./components/Footer/Footer";
-import oAuth                              from "./utils/";
+import Navbar from "./components/NavBar/NavBar";
+import Signup from "./components/Pages/Signup/Signup";
+import Signin from './components/Pages/Login/Login';
+import EarthquakesContainer from './components/Services/EarthquakesContainer';
+import Resources from "./components/Pages/Resources2/Resources2";
+import Footer from "./components/Footer/Footer";
+import oAuth from "./utils/";
 
 class App extends React.Component {
-  state= {
-    oAuth:false
+  state = {
+    oAuth: false
   }
-  handleSignup = (data) =>{
-    oAuth.signup(data).then((res)=>{
+  handleSignup = (data) => {
+    oAuth.signup(data).then((res) => {
       window.localStorage.setItem("token", res.data.token)
-      this.setState({oAuth:true})
-    }).catch(function(error) {
+      this.setState({ oAuth: true })
+    }).catch(function (error) {
       console.log(error);
     });
   }
-  handleSignin = (data) =>{
-    oAuth.signin(data).then((res)=>{
-      window.localStorage.setItem("token",res.data.toke)
-      this.setState({oAuth:true})
-    }).catch(function(error) {
+  handleSignin = (data) => {
+    oAuth.signin(data).then((res) => {
+      window.localStorage.setItem("token", res.data.toke)
+      this.setState({ oAuth: true })
+    }).catch(function (error) {
       alert("Wrong email or password! sucker!")
     });
   }
@@ -33,10 +32,11 @@ class App extends React.Component {
     return (
       <Router>
         <Route path="/" component={Navbar} />
-        <Route exact path="/signup" render={(props) => <Signup {...this.state} {...props} oAuthSignup={this.handleSignup}/>} />
-        <Route exact path="/login" render={(props) => <Signin {...this.state} {...props} oAuthSignin={this.handleSignin}/>} />
-        <Route exact path="/resources" component={Resources}/>
-        <Footer/>
+        <Route exact path="/signup" render={(props) => <Signup {...this.state} {...props} oAuthSignup={this.handleSignup} />} />
+        <Route exact path="/login" render={(props) => <Signin {...this.state} {...props} oAuthSignin={this.handleSignin} />} />
+        <Route exact path="/resources" component={Resources} />
+        <Route exact path="/home" component={EarthquakesContainer} />
+        <Footer />
       </Router>
     )
   }
