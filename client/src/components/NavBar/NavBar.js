553acd93f2a9f2b0a21845baea4d7b0130335769
iromero385 <iromero385@berkeley.edu>
2019-08-06 21:39:10 -0700

    ran it, and it works

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -4,16 +4,6 @@ import './NavBar.css';
 
 const NavBar = (props) => {
     return (
-<<<<<<< HEAD
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand id="Nav">My Fault</Navbar.Brand>
-            <Nav className="button">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/">Resources</Nav.Link>
-            </Nav>
-            <Button variant="outline-primary" href="/login" id="logout">Log Out</Button>
-        </Navbar>
-=======
         <div>
             <Navbar bg="light" variant="light">
                 <Navbar.Brand id="Nav">My Fault</Navbar.Brand>
@@ -24,7 +14,6 @@ const NavBar = (props) => {
                 <Button className="ml-auto" variant="outline-primary" href="/login">Log Out</Button>
             </Navbar>
         </div>
->>>>>>> 9aa444ec0cfa7b4c8dca0471b5538f5fee32944d
     )
 }
 
--- client/src/components/SignupBox/SignupBox.js
+++ client/src/components/SignupBox/SignupBox.js
@@ -5,7 +5,6 @@ import { Col, Form, Button, Row, Container } from 'react-bootstrap';
 
 class SignupBox extends React.Component {
 
-<<<<<<< HEAD
     userInfo = (e) => {
         e.preventDefault();
         const userInfo = {
@@ -15,16 +14,6 @@ class SignupBox extends React.Component {
             password: document.getElementById("formBasicPassword").value
         }
         this.props.oAuthSignup(userInfo);
-=======
-    userInfo(e) {
-        e.preventDefault(); 
-        const userInfo = {
-            firstName:document.getElementById("formBasicFirst").value, 
-            lastName:document.getElementById("formBasicLast").value,
-            email:document.getElementById("formBasicEmail").value,
-            password:document.getElementById("formBasicPassword").value
-        }
->>>>>>> 9aa444ec0cfa7b4c8dca0471b5538f5fee32944d
     }
     render() {
         return (
@@ -34,7 +23,6 @@ class SignupBox extends React.Component {
                         <Col md="8" className="SignBox mt-3">
                             <Form onSubmit={this.userInfo} className="m-2" md={{ span: 4, offset: 8 }}>
                                 <p className="title2">Sign Up</p>
-<<<<<<< HEAD
                                 <Form.Group controlId="formBasicFirst">
                                     <Form.Label className="font">First Name</Form.Label>
                                     <Form.Control type="First Name" placeholder="First Name" />
@@ -48,32 +36,14 @@ class SignupBox extends React.Component {
                                     <Form.Text className="text-muted">
                                         We'll never share your email with anyone else.
                                     </Form.Text>
-=======
-                                <Form.Group controlId="formData">
-                                    <Form.Label className="font">First Name</Form.Label>
-                                    <Form.Control type="First Name" placeholder="First Name" />
-                                    <Form.Label className="font">Last Name</Form.Label>
-                                    <Form.Control type="Last Name" placeholder="Last Name" />
-                                    <Form.Label className="font">Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="example@gmail.com" />
-                                    <Form.Text className="text-muted">
-                                        We'll never share your email with anyone else.
-                        </Form.Text>
->>>>>>> 9aa444ec0cfa7b4c8dca0471b5538f5fee32944d
                                 </Form.Group>
                                 <Form.Group controlId="formBasicPassword">
                                     <Form.Label className="font">Password</Form.Label>
                                     <Form.Control type="password" placeholder="Password" />
                                 </Form.Group>
-<<<<<<< HEAD
                                 <Button onSubmit={this.userInfo} className="signup" variant="primary" type="submit">
                                     Submit
                                 </Button>
-=======
-                                <Button className="signup" variant="primary" type="submit">
-                                    Submit
-                        </Button>
->>>>>>> 9aa444ec0cfa7b4c8dca0471b5538f5fee32944d
                             </Form>
                         </Col>
                     </Row>
