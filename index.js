89e4aa45be0eb582e68c66d16efb10d1695bded4
Becky Ghita <ghita109@gmail.com>
2019-08-08 12:38:29 -0700

    fixing heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors      = require('cors');
 const app = express();
 
 // Database setupx
-mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:auth/auth', { useNewUrlParser: true, useCreateIndex: true});
 
 // Middlewares setup
 app.use(morgan('combined'));
